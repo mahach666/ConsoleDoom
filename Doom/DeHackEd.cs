ae0ece05a02486ebb1815cddddf9e7cfeb1ddb78
Смирнов <maxah66@gmail.com>
2025-04-11 13:35:28 +0300

    CleanAllCore

--- Doom/DeHackEd.cs
+++ Doom/DeHackEd.cs
@@ -1,16 +1,4 @@
-﻿//
-// Copyright (C) 2019-2020 Nobuaki Tanaka
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
+﻿
 
 
 
@@ -409,7 +397,8 @@ namespace ManagedDoom
         {
             foreach (var line in data.Skip(1))
             {
-                var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                //var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var split = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
                 if (split.Length >= 3 && split[0] == "par")
                 {
