ce90f59c8559306358f7b6282068ae829eff9329
Смирнов <maxah66@gmail.com>
2025-04-14 10:10:13 +0300

    wip

--- AppBuilder.cs
+++ AppBuilder.cs
@@ -5,32 +5,37 @@ namespace ConsoleDoom
 {
     public class AppBuilder
     {
-        private DoomApp _app = new DoomApp;
         private string _iwadPath = "DOOM1.WAD";
         private bool _highResolution = false;
         private string[] _extraArgs = Array.Empty<string>();
 
         public AppBuilder SetIwad(string path)
         {
-            _app.IwadPath = path;
+            _iwadPath = path;
             return this;
         }
 
         public AppBuilder EnableHighResolution(bool enable = true)
         {
-            _app.HighResolution = enable;
+            _highResolution = enable;
             return this;
         }
 
         public AppBuilder WithArgs(params string[] args)
         {
-            _app.ExtraArgs = args;
+            _extraArgs = args;
             return this;
         }
 
         public DoomApp Build()
         {
-            return _app;
+            return new DoomApp()
+            {
+                IwadPath = _iwadPath,
+                HighResolution = _highResolution,
+                ExtraArgs = _extraArgs
+            };
         }
     }
 }
+
